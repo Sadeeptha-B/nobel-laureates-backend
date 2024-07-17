988398c6fbbd4598f560613ee58907f7268e8e4b
Sadeeptha-B <sadeeptha.bandara@gmail.com>
2024-07-17 19:54:10 +0530

    Implemented comment posting, getCommentsByLaureateId

--- controllers/comments-controllers.ts
+++ controllers/comments-controllers.ts
@@ -1,6 +1,8 @@
+import { validationResult } from "express-validator";
 import HttpError from "../models/http-error";
-import { Comment } from "./../models/comments";
+import { Comment } from "../models/comment";
 import { RequestHandler } from "express";
+import { randomUUID } from "crypto";
 
 const DUMMY_COMMENTS = [
   {
@@ -15,14 +17,49 @@ const DUMMY_COMMENTS = [
 const getCommentById: RequestHandler = (req, res) => {
   const commentId = req.params.commentId;
 
-  const comment = DUMMY_COMMENTS.find(p => p.id == commentId);
+  const comment = DUMMY_COMMENTS.find((c) => c.id == commentId);
 
-  if (!comment){
-    throw new HttpError(`Comment with ${commentId} does not exist`, 404)
+  if (!comment) {
+    throw new HttpError(`Comment with ${commentId} does not exist`, 404);
   }
 
-  res.json({comment})
+  res.json({ comment });
 };
 
+const getCommentsByLaureateId: RequestHandler = (req, res) => {
+  const laureateId = req.params.laureateId;
 
-export {getCommentById}
+  const comments = DUMMY_COMMENTS.filter((c) => c.laureateId == laureateId);
+  res.json({ comments: [] });
+};
+
+const postComment: RequestHandler = (req, res, next) => {
+  // TODO: DRY with usercontroller. make util. However wait till db is integrated
+  // Note that if the code is async, error handling should happen with the next function
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty) {
+    throw new HttpError(
+      `${errors
+        .array()
+        .map((e) => e.msg)
+        .join(",")}`,
+      422
+    );
+  }
+
+  const { userId, laureateId, content, timePosted } = req.body as Comment;
+
+  const newComment = {
+    id: randomUUID(),
+    userId,
+    laureateId,
+    content,
+    timePosted,
+  };
+
+  DUMMY_COMMENTS.push(newComment);
+  res.status(201).json({ comment: newComment });
+};
+
+export { getCommentById, postComment, getCommentsByLaureateId };
--- routes/comments-routes.ts
+++ routes/comments-routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import * as commentsController from '../controllers/comments-controllers';
+import * as commentsController from "../controllers/comments-controllers";
+import { check } from "express-validator";
 
-const commentsRouter = Router()
+const commentsRouter = Router();
 
-commentsRouter.get('/:commentId', commentsController.getCommentById)
-
+commentsRouter.get("/:commentId", commentsController.getCommentById);
+commentsRouter.get("/:laureateId", commentsController.getCommentsByLaureateId);
+commentsRouter.post("/", [
+  check("content")
+    .not()
+    .isEmpty()
+    .withMessage("Comment content cannot be empty"),
+], commentsController.postComment);
 
 export default commentsRouter;
